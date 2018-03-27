be039aa9cb42ad18c024b762ccb1d989db833690
Mikkel Oerberg Hansen <mikkeloerberg.hansen@my.jcu.edu.au>
2018-03-27 10:03:55 +0800

    added error checking

--- oddName.py
+++ oddName.py
@@ -1,4 +1,18 @@
 """
-Mikkel Ørberg Hansen
+Mikkel Hansen
 """
 
+
+def main():
+    name = str(input("input your name: "))
+
+    for char in name:
+        print(char, char.isalpha())
+
+    if all(x.isalpha() or x.isspace() for x in name):
+        print("Only alphabetical letters and spaces: yes")
+    else:
+        print("Only alphabetical letters and spaces: no")
+
+
+main()
